1c679fba563cf50663c17bb4812a79f30da8ee4a
elena-elsa101 <elenaelsacherian@gmail.com>
2026-03-28 22:02:14 +0530

    Update SubarraysDivByK.java

--- src/SubarraysDivByK.java
+++ src/SubarraysDivByK.java
@@ -1,7 +1,19 @@
+    import java.util.*;
 public class SubarraysDivByK {
-    
-        // TODO: Read n and k
-        // TODO: Read array elements
-        // TODO: Count and print the number of subarrays with sum divisible by k
-    
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt(),k=sc.nextInt();
+int[] arr=new int[n];
+for(int i=0;i<n;i++) arr[i]=sc.nextInt();
+int[] freq=new int[k];
+freq[0]=1;
+int sum=0,count=0;
+for(int i=0;i<n;i++){
+sum+=arr[i];
+int rem=((sum%k)+k)%k;
+count+=freq[rem];
+freq[rem]++;
+}
+System.out.print(count);
+}
 }
